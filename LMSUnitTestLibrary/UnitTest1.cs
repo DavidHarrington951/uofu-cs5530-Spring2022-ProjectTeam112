03399ab779cfe7ad4b29ca6f793cc98654251915
David Harrington <david.harrington2095@gmail.com>
2022-04-01 12:44:35 -0600

    Added Project References, Added Base Test,

--- LMSUnitTestLibrary/UnitTest1.cs
+++ LMSUnitTestLibrary/UnitTest1.cs
@@ -1,14 +1,40 @@
-using System;
 using Xunit;
+using LMS.Controllers;
+using LMS.Models.LMSModels;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace LMSUnitTestLibrary
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
 
+        private static ServiceProvider NewServiceProvider()
+        {
+            ServiceProvider serviceProvider = new ServiceCollection()
+              .AddEntityFrameworkInMemoryDatabase()
+              .BuildServiceProvider();
+            return serviceProvider;
         }
+
+        [Fact]
+        public void TestDepartmentQueryType()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Team112LMSContext>();
+            optionsBuilder.UseInMemoryDatabase("SmallLms").UseApplicationServiceProvider(NewServiceProvider());
+
+            Team112LMSContext context = new Team112LMSContext(optionsBuilder.Options);
+
+            CommonController controller = new CommonController();
+            JsonResult result = controller.GetDepartments() as JsonResult;
+
+            var x = result.Value;
+            Type valueType = x.GetType();
+            Assert.True(valueType.IsArray);
+        }
+
     }
 }
