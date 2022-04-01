2a19df7eac85913756dee29bcdd2225280a759ed
David Harrington <david.harrington2095@gmail.com>
2022-04-01 13:12:45 -0600

    3 basic unit tests working, Use of reflection to get anonymous type information

--- LMSUnitTestLibrary/UnitTest1.cs
+++ LMSUnitTestLibrary/UnitTest1.cs
@@ -20,21 +20,70 @@ namespace LMSUnitTestLibrary
             return serviceProvider;
         }
 
-        [Fact]
-        public void TestDepartmentQueryType()
+        public Team112LMSContext PopulateEmptyDatabase()
         {
             var optionsBuilder = new DbContextOptionsBuilder<Team112LMSContext>();
             optionsBuilder.UseInMemoryDatabase("SmallLms").UseApplicationServiceProvider(NewServiceProvider());
-
             Team112LMSContext context = new Team112LMSContext(optionsBuilder.Options);
 
-            CommonController controller = new CommonController();
+            return context;
+        }
+
+        public Team112LMSContext PopulateSingleDepartment()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<Team112LMSContext>();
+            optionsBuilder.UseInMemoryDatabase("SmallLms").UseApplicationServiceProvider(NewServiceProvider());
+            Team112LMSContext context = new Team112LMSContext(optionsBuilder.Options);
+
+            Departments department = new Departments
+            {
+                DprtAbv = "CS",
+                DprtName = "Computer Science"
+            };
+            context.Departments.Add(department);
+            context.SaveChanges();
+
+            return context;
+        }
+
+        [Fact]
+        public void TestDepartmentQueryType()
+        {
+            Team112LMSContext context = PopulateEmptyDatabase();
+            CommonController controller = new CommonController(context);
             JsonResult result = controller.GetDepartments() as JsonResult;
 
             var x = result.Value;
             Type valueType = x.GetType();
             Assert.True(valueType.IsArray);
         }
+
+        [Fact]
+        public void TestEmptyTables()
+        {
+            Team112LMSContext context = PopulateEmptyDatabase();
+            CommonController controller = new CommonController(context);
+
+            JsonResult result = controller.GetDepartments() as JsonResult;
+            dynamic x = result.Value;
+            Assert.Equal(0, x.Length);
+        }
+
+        [Fact]
+        public void TestSingleTable()
+        {
+            Team112LMSContext context = PopulateSingleDepartment();
+            CommonController controller = new CommonController(context);
+
+            JsonResult result = controller.GetDepartments() as JsonResult;
+            dynamic x = result.Value;
+            Assert.Equal(1, x.Length);
+
+            Type type = x[0].GetType();
+            String abv = (String)type.GetProperty("subject").GetValue(x[0], null);
+        }
+
+
 
     }
 }
