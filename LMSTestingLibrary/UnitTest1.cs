d32913690f1244f02689676943d7d2712bb26a28
David Harrington <david.harrington2095@gmail.com>
2022-04-01 12:12:43 -0600

    Temporary Fix to testing suite setup

--- LMSTestingLibrary/UnitTest1.cs
+++ LMSTestingLibrary/UnitTest1.cs
@@ -3,6 +3,7 @@ using LMS.Models.LMSModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using Xunit;
 
@@ -10,21 +11,26 @@ namespace LMSTestingLibrary
 {
     public class UnitTest1
     {
-        [Fact]
-        public void TestDepartmentsType()
+        public Team112LMSContext generateSmallLms()
         {
             var optionsBuilder = new DbContextOptionsBuilder<Team112LMSContext>();
-            optionsBuilder.UseInMemoryDatabase("SmallLms").UseApplicationServiceProvider();
+            optionsBuilder.UseInMemoryDatabase("SmallLms");
 
             Team112LMSContext context = new Team112LMSContext(optionsBuilder.Options);
 
-            CommonController controller = new CommonController();
+            return context;
+        }
+
+        [Fact]
+        public void TestDepartmentsType()
+        {
+            Team112LMSContext smallContext = generateSmallLms();
+            CommonController controller = new CommonController(smallContext);
             JsonResult result = controller.GetDepartments() as JsonResult;
 
             var x = result.Value;
             Type valueType = x.GetType();
             Assert.True(valueType.IsArray);
-            //we won't test the element type since it is an anonymous type
         }
 
         [Fact]
